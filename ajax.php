90b7c3506771069d67a71c0b616226ce66c6ba8e
gamboss <erdemgomboha@gmail.com>
2016-10-14 03:51:42 +0000

    Update ajax.php
    added new validation rules

--- ajax.php
+++ ajax.php
@@ -1,66 +1,73 @@
 <?php
 	$message = null;
 	$error = null;
-	$hash = null;
 
+	//validation for add admin form
 	$admin_login = $_POST['postLogin'];
 	$admin_password = $_POST['postPassword'];
+	$hash = null;
 
-	//Write new data into JSON file
-	if (isset($admin_login)) {
-		if (empty($admin_login)) {
-			$error = "<p class='text-danger'>Введите логин</p>";
-		}
-		else if (empty($admin_password)) {
-			$error = "<p class='text-danger'>Введите пароль</p>";
-		}
-		else {
-			if (file_exists('json/admin_list.json')) {
-				$current_data = file_get_contents('json\admin_list.json');
-				$array_data = json_decode($current_data, true);				
-				
-				//login check exist
-				foreach ($array_data as $check) {
-					$check_name = $check['admin_name'];
-					if ($admin_login == $check_name) {
-						$error = "<p class='text-danger'>Пользователь с таким именем уже существует!</p>";
-					}
-				}				
+	if (empty($admin_login)) {
+		$error = "<p class='text-danger'>Введите логин!</p>";
+	}
+	else if (preg_match('/[^A-Za-z0-9]/', $admin_login)) {
+		$error = "<p class='text-danger'>Логин должен сожержать только латинские символы и буквы!</p>";
+	}
+	else if (strlen($admin_login) > 16) {
+		$error = "<p class='text-danger'>Логин должен содержать максимум 16 символов!</p>";
+	}
+	else if (empty($admin_password)) {
+		$error = "<p class='text-danger'>Введите пароль!</p>";
+	}
+	else if (strlen($admin_password) < 5) {
+		$error = "<p class='text-danger'>Пароль должен содержать не менее 5 символов!</p>";
+	}
+	else {
+		if (file_exists('json/admin_list.json')) {
+			$current_data = file_get_contents('json\admin_list.json');
+			$array_data = json_decode($current_data, true);				
+			
+			//login check exist
+			foreach ($array_data as $check) {
+				$check_name = $check['admin_name'];
+				if ($admin_login == $check_name) {
+					$error = "<p class='text-danger'>Пользователь с таким именем уже существует!</p>";
+				}
+			}				
 
-				if ($error == null) {
-					// Generate auto increment ID
-					$tmp = 0;
-					foreach ($array_data as $value) {
-						$output = $value['id'];
-						if ($tmp < $output) {
-							$tmp = $output;
-						}
-					}
-					$new_id = $tmp + 1;
-
-					//password hash
-					$hash = password_hash($admin_password, PASSWORD_BCRYPT, array(
-						'cost' => 12
-					));
-					
-					$extra = array(
-						'id' => 			$new_id,
-						'admin_name' => 	$admin_login,
-						'admin_password' =>	$hash
-					);
-
-					$array_data[] = $extra;
-					$final_data = json_encode($array_data);
-
-					if (file_put_contents('json\admin_list.json', $final_data)) {
-						$message = "<p class='text-success'>Пользователь добавлен</p>";
+			if ($error == null) {
+				// Generate auto increment ID
+				$tmp = 0;
+				foreach ($array_data as $value) {
+					$output = $value['id'];
+					if ($tmp < $output) {
+						$tmp = $output;
 					}
 				}
+				$new_id = $tmp + 1;
+
+				//password hash
+				$hash = password_hash($admin_password, PASSWORD_BCRYPT, array(
+					'cost' => 12
+				));
+				
+				$extra = array(
+					'id' => 			$new_id,
+					'admin_name' => 	$admin_login,
+					'admin_password' =>	$hash
+				);
+
+				$array_data[] = $extra;
+				$final_data = json_encode($array_data);
+
+				if (file_put_contents('json\admin_list.json', $final_data)) {
+					$message = "<p class='text-success'>Пользователь добавлен</p>";
+				}
 			}
-			else {
-				$error = "<p class='text-danger'>JSON-файл не существует</p>";
-			}			
 		}
+		else {
+			$error = "<p class='text-danger'>JSON-файл не существует</p>";
+		}			
 	}
 
 	//send ajax response to js
